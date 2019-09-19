9685475cead4b75b8e75e3a4bd1bd38b7a6467ce
Johan Jokijärvi <johan.jokijarvi@learnet.se>
2019-09-19 11:17:50 +0200

    uppgifter kap2

---
+++ Uppg3.java
@@ -0,0 +1,16 @@
+/*
+uppg 2.6
+*/
+import javax.swing.*;
+import java.util.Scanner;
+
+public class Uppg3 {
+		public static void main (String[] args)	{
+			char c1 = '\u00D8';
+			char c2 = '\u00F8';
+			
+			JOptionPane.showMessageDialog(null, c1);
+			JOptionPane.showMessageDialog(null, c2);
+			JOptionPane.showMessageDialog(null, c1 + "\n" + c2);
+		}
+}	
